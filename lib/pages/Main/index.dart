dea2e8a0fccce89d985ca4cf99d3302e571fe4fa
dongtao <dongtaocom@126.com>
2026-03-23 21:27:59 +0800

    routes->login->main tabbarItem

--- lib/pages/Main/index.dart
+++ lib/pages/Main/index.dart
@@ -8,11 +8,60 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  //定义数据根据数据进行渲染4个导航
+  //一般应用程序导航是固定的额
+  final List<Map<String,String>> _tabList = [
+    {
+    "icon":"lib/assets/home_n.png",
+    "active_icon":"lib/assets/home_a.png",
+    "text":"首页",
+     },
+    {
+    "icon":"lib/assets/list_n.png",
+    "active_icon":"lib/assets/list_a.png",
+    "text":"分类",
+    },
+    {
+      "icon":"lib/assets/car_n.png",
+      "active_icon":"lib/assets/car_a.png",
+      "text":"购物车",
+    },
+    {
+      "icon":"lib/assets/home_n.png",
+      "active_icon":"lib/assets/home_a.png",
+      "text":"我的",
+    },
+
+  ];
+  int _currentIndex = 0;
+  List<BottomNavigationBarItem>_getTabBarWidget(){
+   return  List.generate(_tabList.length, (int index){
+     return BottomNavigationBarItem(
+       icon: Image.asset(_tabList[index]["icon"]!,width: 30,height: 30),
+       activeIcon: Image.asset(_tabList[index]["active_icon"]!,width: 30,height: 30,),
+       label: _tabList[index]["text"],
+     );
+
+   });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold
-      (appBar: AppBar(title: Text("主页")),
+      (
       body: Center(child:Text("主页")),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (int index){
+          _currentIndex = index;
+          setState(() {});
+        },
+        items: _getTabBarWidget(),
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.black,
+        type: BottomNavigationBarType.fixed,
+      ),
+
     );
   }
 }
