478376d6c901fe41a1146c4ad23e1b4e3d362570
dongtao <dongtaocom@126.com>
2026-03-30 16:38:24 +0800

    登录页çé页面

--- lib/pages/Login/index.dart
+++ lib/pages/Login/index.dart
@@ -1,21 +1,186 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/utils/ToastUtils.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  LoginPage({Key? key}) : super(key: key);
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController _phoneController = TextEditingController(); // 账号控制器
+  TextEditingController _codeController = TextEditingController(); // 密码控制器
+  // 用户账号Widget
+  Widget _buildPhoneTextField() {
+    return TextFormField(
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return "手机号不能为空";
+        }
+        //校验手机号格式
+        RegExp regExp = RegExp(r'^1[3456789]\d{9}$');
+        if (!regExp.hasMatch(value)) {
+          return "手机号格式不正确";
+        }
+        return null;
+      },
+      controller: _phoneController,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.only(left: 20), // 内容内边距
+        hintText: "请输入账号",
+        fillColor: const Color.fromRGBO(243, 243, 243, 1),
+        filled: true,
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.circular(25),
+        ),
+      ),
+    );
+  }
+
+  // 用户密码Widget
+  Widget _buildCodeTextField() {
+    return TextFormField(
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return "密码不能为空";
+        }
+        if(!RegExp(r'^[a-zA-Z0-9]{6,20}$').hasMatch(value)){
+          return "密码格式不正确";
+        }
+        return null;
+      },
+      controller: _codeController,
+      obscureText: true,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.only(left: 20), // 内容内边距
+        hintText: "请输入密码",
+        fillColor: const Color.fromRGBO(243, 243, 243, 1),
+        filled: true,
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.circular(25),
+        ),
+      ),
+    );
+  }
+
+  // 登录按钮Widget
+  Widget _buildLoginButton() {
+    return SizedBox(
+      width: double.infinity,
+      height: 50,
+      child: ElevatedButton(
+        onPressed: () {
+          // 登录逻辑
+          if(_key.currentState!.validate()){
+            //进行勾选判断
+            if(_isChecked){
+              //勾选判断
+            }else{
+              ToastUtils.showToast(context, "请勾选用户协议");
+            }
+          }
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.black,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25),
+          ),
+        ),
+        child: Text("登录", style: TextStyle(fontSize: 18, color: Colors.white)),
+      ),
+    );
+  }
+
+  bool _isChecked = false;
+  // 勾选Widget
+  Widget _buildCheckbox() {
+    return Row(
+      children: [
+        // 设置勾选为圆角
+        Checkbox(
+          value: _isChecked,
+          activeColor: Colors.black,
+          checkColor: Colors.white,
+          onChanged: (bool? value) {
+            _isChecked = value ?? false;
+            setState(() {
+
+            });
+          },
+          // 设置形状
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10), // 圆角大小
+          ),
+          // 可选：设置边框
+          side: BorderSide(color: Colors.grey, width: 2.0),
+        ),
+        Text.rich(
+          TextSpan(
+            children: [
+              TextSpan(text: "查看并同意"),
+              TextSpan(
+                text: "《隐私条款》",
+                style: TextStyle(color: Colors.blue),
+              ),
+              TextSpan(text: "和"),
+              TextSpan(
+                text: "《用户协议》",
+                style: TextStyle(color: Colors.blue),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  // 头部Widget
+  Widget _buildHeader() {
+    return Row(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: 10),
+          child: Text(
+            "账号密码登录",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ],
+    );
+  }
+
+  final GlobalKey<FormState> _key = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: AppBar(
-         title: Text("登录"),
-       ),
-      body: Center(
-        child: Text("登录页"),
+      appBar: AppBar(
+        title: Text("登 录", style: TextStyle(fontWeight: FontWeight.bold)),
+        backgroundColor: Colors.white,
+      ),
+      body: Form(
+        key:_key,
+        child: Container(
+          padding: EdgeInsets.all(30),
+          color: Colors.white,
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              _buildHeader(),
+              SizedBox(height: 30),
+              _buildPhoneTextField(),
+              SizedBox(height: 20),
+              _buildCodeTextField(),
+              SizedBox(height: 20),
+              _buildCheckbox(),
+              SizedBox(height: 20),
+              _buildLoginButton(),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/utils/ToastUtils.dart
+++ lib/utils/ToastUtils.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 
 class ToastUtils {
+  static bool showLoading = false;
   static void showToast(BuildContext context, String message) {
+    if(ToastUtils.showLoading){
+      return;
+    }
+    ToastUtils.showLoading = true;
+    Future.delayed(Duration(seconds: 3),(){
+      ToastUtils.showLoading = false;
+    });
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        width: 100,
+        width: 180,
         content: Text(
           message ?? "加载成功",
           textAlign: TextAlign.center,  // 文字居中
         ),
 
-        duration: Duration(seconds: 5),
+        duration: Duration(seconds: 3),
         // 可选：调整背景色、边距等
         backgroundColor: Colors.black87,
         behavior: SnackBarBehavior.floating, // 浮动样式更明显
